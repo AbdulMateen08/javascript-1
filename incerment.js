412be3f1ce56ac9003cb6410360d93549ff4f7b6
Mateen <mdmateen147@gmail.com>
2024-06-14 10:07:44 +0530

    increment is commited

---
+++ incerment.js
@@ -0,0 +1,22 @@
+// post increment 
+// let a= 5
+// let b=a++  in is the value is first assign to b then it will increment the a value
+// console.log(a)
+// console.log(b)
+//pre increment
+// let x=5
+// let y=++x  in this the value is first increment then the vlaue is assign
+// console.log(x)
+// console.log(y)
+        
+// pre Decrement 
+// let a=5
+// let b=--a in this the value is first decremented then the value is assign
+// console.log(a);
+// console.log(b);
+
+//post Decrement
+// let x=5
+// let y=x-- in this the value is first is assign then the decrement is done
+// console.log(x);
+// console.log(y);
