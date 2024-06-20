502009d9c688a97961ab53d407f8ef38338d9f62
Mateen <mdmateen147@gmail.com>
2024-06-20 12:41:41 +0530

    IFFE functions are added

---
+++ Iffefunction.js
@@ -0,0 +1,21 @@
+//Immediatly invoke function expression
+
+( function greeting(){   // Named IIFE 
+    console.log("GoodMorning");
+})();
+
+
+
+( ()=>{
+    console.log("GoodAfternoon");  // Unnamed IIFE using arrow function
+
+})();                         
+    
+
+
+((name)=>{                        // IIFE using Arrow Function with  passing value
+    console.log(`Good Afternoon ${name}`);
+})("mateen");
+
+let num=  4 ?? null
+console.log(num)
